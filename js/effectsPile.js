c2bbebde83d991e23a88d010f17ce36f6fff0a42
Olivier Beguin <olivier.beguin.1@ens.etsmtl.ca>
2018-02-11 23:53:35 -0500

    Added a delete button
    Each effect entry has a working delete button

--- js/effectsPile.js
+++ js/effectsPile.js
@@ -56,17 +56,23 @@ function showEffects ()
     for (var i = 0; i < effectsPile.length; i++) {
         var effect = effectsPile[i];
 
-        switch (effect.type)
-        {
+        switch (effect.type) {
             case "grayscale":
-                effectsList.innerHTML += "<li>Grayscale ; " + effect.rShift + " ; " + effect.gShift + " ; " + effect.bShift + "</li>";
+                effectsList.innerHTML += "<li>Grayscale ; " + effect.rShift + " ; " + effect.gShift + " ; " + effect.bShift + "<button class='deleteBtn' onclick='deleteEffect(" + i + ")'>X</button></li>";
                 break;
 
             case "random":
-                effectsList.innerHTML += "<li>Random ; " + effect.opacity + "</li>";
+                effectsList.innerHTML += "<li>Random ; " + effect.opacity + "<button class='deleteBtn' onclick='deleteEffect(" + i + ")'>X</button></li>";
                 break;
         }
     }
+}
+
+function deleteEffect(numero)
+{
+    effectsPile.splice(numero, 1);
+    applyEffects();
+    showEffects();
 }
 
 function clearPile()
